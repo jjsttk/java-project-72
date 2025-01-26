7c32a9e29d1c555560c4009c26173509968235ee
Kraizlol <kraizlol@mail.ru>
2025-01-26 16:19:07 +0400

    test mainCheck.yml

--- app/src/main/java/hexlet/code/Main.java
+++ app/src/main/java/hexlet/code/Main.java
@@ -2,6 +2,8 @@ package hexlet.code;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+    public static String helloWorld() {
+        return "Hello world!";
     }
 }
